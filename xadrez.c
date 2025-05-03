85d976e5b23a41ffc01a7c5bcd45c0e32ec73f21
irwin40 <irwinbrandao@gmail.com>
2025-05-03 21:14:31 +0000

    Nível Novato

--- xadrez.c
+++ xadrez.c
@@ -1,32 +1,29 @@
 #include <stdio.h>
 
-// Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
+int main(){
+    int t = 1, b = 1, r = 1;
 
-int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+//Torre
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+printf("\n\n-- Movimentação da Torre (5 casas para direita) -- \n\n");
+for (int t = 1; t <= 5; t++){
+    printf("Direita --> || ");
+    }    
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+//Bispo
 
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+printf("\n\n\n-- Movimentação do Bispo (5 casas na diagonal para cima e à direita-- \n\n");
+while (b <= 5){
+    printf("Direita --> Cima ^^ || ");
+    b++;} 
 
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
-
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
-
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
-
-    return 0;
+//Rainha
+ 
+printf("\n\n\n-- Movimentação da Rainha (8 casas para a esquerda) -- \n\n");
+    do {
+        printf("<-- Esquerda ||");
+        r++;
+    } while (r <= 8);
+printf("\n\n");    
+return 0;
 }
